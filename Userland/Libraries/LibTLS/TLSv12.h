cfa869f63db9163e5e23968da9a434dd4dc05435
Michiel Visser <opensource@webmichiel.nl>
2022-02-21 22:25:34 +0100

    LibTLS: Verify the certificate chain sent by the server
    With this change the certificate chain sent by the server will actually
    be verified, instead of just checking the names of the certificates.
    
    To determine if a certificate is signed by a root certificate, the list
    of root certificates is now a HashMap mapping from the unique identifier
    string to the certificate. This allows us to take the issuer of a
    certificate and easily check if it is a root certificate. If a
    certificate is not signed by a root certificate, we will check that it
    is signed by the next certificate in the chain.
    
    This also removes the ad-hoc checking of certificate validity from
    multiple places, and moves all checking to the verify_chain.

--- Userland/Libraries/LibTLS/TLSv12.h
+++ Userland/Libraries/LibTLS/TLSv12.h
@@ -258,6 +258,7 @@ struct Options {
 
 struct Context {
     bool verify_chain() const;
+    bool verify_certificate_pair(Certificate& subject, Certificate& issuer) const;
 
     Options options;
 
@@ -317,7 +318,7 @@ struct Context {
     // message flags
     u8 handshake_messages[11] { 0 };
     ByteBuffer user_data;
-    Vector<Certificate> root_certificates;
+    HashMap<String, Certificate> root_certificates;
 
     Vector<String> alpn;
     StringView negotiated_alpn;
